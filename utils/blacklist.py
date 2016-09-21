47403c771aab619f28a14c213696554457ebdcc5
wqhhust <wuqihua_it@163.com>
2016-09-22 07:50:57 +0800

    commit

--- utils/blacklist.py
+++ utils/blacklist.py
@@ -5,22 +5,20 @@ session_requests = requests.session()
 login_url = "https://ac.ppdai.com/User/Login?redirect="
 result = session_requests.get(login_url)
 tree = html.fromstring(result.text)
-
 config = configparser.ConfigParser()
-config.read("./../blacklist.config")
+config.read("./blacklist.config")
 user_config = config["user"]
 print(tree)
 payload = {
 	"UserName": user_config["username"],
 	"Password": user_config["password"]
 }
-print(payload)
 result = session_requests.post(
 	login_url,
 	data = payload,
 	headers = dict(referer=login_url)
 )
-print(result.text)
 result = session_requests.get("http://invest.ppdai.com/account/blacklist")
-print(result.text)
+
+tree = html.fromstring(result.text)
 
