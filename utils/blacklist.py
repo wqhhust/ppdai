ac39eb0b066f27883efcebf64609c6110b0bae0d
wqhhust <wuqihua_it@163.com>
2016-09-24 21:43:27 +0800

    commit

--- utils/blacklist.py
+++ utils/blacklist.py
@@ -44,14 +44,14 @@ def get_delayed_bidding(url):
     for tr in trs:
         tds = tr.xpath(".//td")
         if len(tds) > 0:
-            td_money = tds[1].text
+            td_money = tds[1].text.replace(",","")
             money = [float(x) for x in (re.compile("[0-9.]+").findall(td_money))]
-            print(td_money,money)
-            td_delay_days=tds[3].text
+            #print(td_money,money)
+            td_delay_days=tds[3].text.replace(",","")
             delay_days = [int(x) for x in (re.compile("\d+").findall(td_delay_days))]
-            print(td_delay_days,delay_days)
+            #print(td_delay_days,delay_days)
             bidding_id = tds[0].findall("./span")[1].attrib["listingid"]
-            print(bidding_id)
+            #print(bidding_id)
             yield [bidding_id]+money+delay_days
 
 
@@ -59,13 +59,16 @@ def get_page_url(pagecount):
     return "http://invest.ppdai.com/account/blacklist?PageIndex={}&IsCalendarRequest=0".format(pagecount)
 
 
+pg_cursor.execute("delete from ppdai_blacklist where user_name=''".format(ppdai_username))
 pages_count = get_pages()
 for x in range(pages_count):
     url = get_page_url(x+1)
     for row in get_delayed_bidding(url):
+        print(row)
         row.insert(0, ppdai_username)
         row_tuple = tuple(row)
-        sql = "insert into ppdai_blacklist values ({},{},{},{},{},{},{})".format(*row_tuple)
+        sql = """insert into ppdai_blacklist (user_name,bidding_id,overdue,returned,total,overdue_days,max_overdue_days)
+         values ({},{},{},{},{},{},{})""".format(*row_tuple)
         print(sql)
         pg_cursor.execute(sql)
 
