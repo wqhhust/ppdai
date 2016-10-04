b57ef5348f8a8aee54b54108ddc40c4fc0d6c704
wqhhust <wuqihua_it@163.com>
2016-10-04 13:21:25 +0800

    commit

--- utils/bidding.py
+++ utils/bidding.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+import threading
 import time
 import pickle
 import requests
@@ -57,6 +58,7 @@ def get_bidding_list_after_loggin(session,url):
     bidding_elements = tree.findall('.//a[@class="title ell"]')
     bidding_id_list = [x.attrib["href"].split("=")[1] for x in bidding_elements]
     print(bidding_id_list)
+    return bidding_id_list
 
 
 def get_bidding_details(session,bidding_id):
@@ -128,6 +130,12 @@ def test():
 def consume_queue():
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
+        page = re.sub(".*:page-number|,|:timestamp.*", "", str(body)).strip()
+        url_template = "http://invest.ppdai.com/loan/listnew?LoanCategoryId=4&SortType=2&PageIndex=3&MinAmount=0&MaxAmount=0"
+        url = url_template.format(page)
+        get_bidding_list_after_loggin(s, url)
+        print(url)
+
     url_params = "amqp://ppdai:ppdai2016@123.206.203.97"
 
     parameters = pika.URLParameters(url_params)
@@ -159,5 +167,10 @@ def consume_queue():
     except KeyboardInterrupt:
         channel.stop_consuming()
     connection.close()
+
+
+load_cookie_to_requests(s, file)
+t = threading.Thread(target=consume_queue)
+t.start()
 
 page = test()
