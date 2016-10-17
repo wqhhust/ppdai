5b5458f9fb561de579080880b4930c1545f20077
Daniel.Wu <wuqihua_it@163.com>
2016-10-17 21:47:14 +0800

    firefox time out setting

--- utils/bidding.py
+++ utils/bidding.py
@@ -15,6 +15,7 @@ from lxml import html
 from multiprocessing import Process
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 import utils
 headers = {
@@ -347,9 +348,13 @@ def prepare_db():
 
 def do_bidding(driver,bidding_id,amount):
     url = "http://invest.ppdai.com/loan/info?id={}".format(bidding_id)
-    print(driver)
+    driver.set_page_load_timeout(10)
     logger_to_broadcast.info("start bidding for {}".format(bidding_id))
-    driver.get(url)
+    try:
+        driver.get(url)
+    except Exception:
+        logger_to_broadcast.info("getting page content time out, stop loading the page")
+        driver.send_keys(Keys.CONTROL +'Escape')
     logger_to_broadcast.info("got the page for bidding of {}".format(bidding_id))
     try:
         driver.find_element_by_class_name('expquickbid')
