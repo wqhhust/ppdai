3017751b8f34fdf705e9805d1322f905b6e653ec
Daniel.Wu <wuqihua_it@163.com>
2016-11-09 21:53:08 +0800

    cc

--- utils/runppdai.sh
+++ utils/runppdai.sh
@@ -2,9 +2,9 @@ while [ 1 -eq 1 ]
 do
     echo "start ppdai python ......"
     python3 bidding.py &
-    sleep 60
     sleep 2400
     ps -ef|grep bidding |grep -v grep|awk {'print $2'}|xargs kill -9
     ps -ef|grep firefox|grep -v grep|awk {'print $2'}|xargs kill -9
     echo "killed ppdai python...."
+    sleep 30
 done
